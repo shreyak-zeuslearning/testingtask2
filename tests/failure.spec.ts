a802210907b8c7bca31191922b2986baa2f802ad
shreyak-zeuslearning <shreya.kathe@zeuslearning.com>
2026-06-12 16:12:26 +0530

    changes

--- tests/failure.spec.ts
+++ tests/failure.spec.ts
@@ -2,43 +2,43 @@ import {test,expect} from '@playwright/test';
 import {link} from '../utils/weblinks';
 
 // failure 1 
-test('Locator failure', async({page})=>{
+test('01 Locator failure', async({page})=>{
   await page.goto(link.booklink);
   await page.locator('#wrong-id').click();
 });
 
 // failure 2 
-test('Assertion failure',async({page})=>{
+test('02 Assertion failure',async({page})=>{
   await page.goto(link.booklink);
   await expect(page).toHaveTitle('Zeus');
 })
 
 // failure 3 
-test('Element not visible',async({page})=>{
+test('03 Element not visible',async({page})=>{
   await page.goto(link.booklink);
   await expect (page.getByText('Random text')).toBeVisible();
 })
 
 // //failure 4 
- test('Navigation failure',async({page})=>{
+ test('04 Navigation failure',async({page})=>{
       await page.goto('https://wrongdemoqa.com/books')
  })
 
 // failure 5
-test('Closed Page Usage',async({page})=>{
+test('05 Closed Page Usage',async({page})=>{
   await page.goto(link.booklink)
    await page.close();
    await page.click('button');
 })
 
 // failure 6
-test('Strict mode failure',async({page})=>{
+test('06 Strict mode failure',async({page})=>{
   await page.goto(link.booklink);
   await page.locator('a').isVisible();
 });
 
 // failure 7 
-test('Text verification failure',async({page})=>{
+test('07 Text verification failure',async({page})=>{
       await page.goto('https://www.saucedemo.com/');
     
       await expect(page.locator('#login-button')).toHaveText('Zeus');
@@ -46,7 +46,7 @@ test('Text verification failure',async({page})=>{
 })
 
 //failure 8 
-test('Disabled Element failure ',async({page})=>{
+test('08 Disabled Element failure ',async({page})=>{
 
   await page.setContent(`
     <button id="submit"disabled> Submit </button>`
